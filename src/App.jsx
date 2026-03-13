de563da035026ecfc0fdfb4a15cb013bfebeac66
developer36chirag-create <developer36chirag@gmail.com>
2026-03-13 16:40:37 +0530

    Fix router issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Future pages can be added here */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
